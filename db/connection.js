e832e78329051f7a20597054cd556994e80d8044
Tristan Bond <tristanebond@gmail.com>
2020-12-30 12:25:16 -0800

    connection module file

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+const util = require("util");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: "root",
+    password: "cheetah4",
+
+    database: "employees"
+});
+
+connection.connect((err) => {
+
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
+
+connection.query = util.promisify(connection.query);
+
+module.exports=connection;
